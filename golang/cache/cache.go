95147c97cb2d782d1afcf1762e71d513bcb7d35c
Fuyang Liu <fuyangl@spotify.com>
2020-10-01 20:56:47 +0200

    Add simple expire logic

--- golang/cache/cache.go
+++ golang/cache/cache.go
@@ -1,6 +1,17 @@
 package cache
 
-import "golang.org/x/sync/syncmap"
+import (
+	"time"
+
+	"golang.org/x/sync/syncmap"
+)
+
+const ExpireDurationStr = "5s"
+
+type Value struct {
+	value     string
+	timestamp time.Time
+}
 
 type Cache interface {
 	Get(key string) string
@@ -16,11 +27,21 @@ func NewSyncmapCache() SyncmapCache {
 }
 
 func (sc *SyncmapCache) Get(key string) string {
-	v, _ := sc.m.Load(key)
-	vStr, _ := v.(string)
-	return vStr
+	v, ok := sc.m.Load(key)
+	if !ok {
+		return ""
+	}
+	value := v.(Value)
+
+	expireDuration, _ := time.ParseDuration(ExpireDurationStr)
+	if time.Now().After(value.timestamp.Add(expireDuration)) {
+		sc.m.Delete(key)
+		return ""
+	}
+	return value.value
 }
 
 func (sc *SyncmapCache) Set(key string, value string) {
-	sc.m.Store(key, value)
+	v := Value{value: value, timestamp: time.Now()}
+	sc.m.Store(key, v)
 }
