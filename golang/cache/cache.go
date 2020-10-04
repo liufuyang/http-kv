ee1820c633b3592a1ad5cdf091a7e6050f1cf22b
Fuyang Liu <fuyangl@spotify.com>
2020-10-05 00:48:04 +0200

    Simplify vaccum code and further reduce key loop sleeping time

--- golang/cache/cache.go
+++ golang/cache/cache.go
@@ -86,13 +86,28 @@ func (sc *SyncmapCache) SizePrecise() int {
 }
 
 // vaccum method is used for SyncmapCache clean up expired key
-// We set up a minimum sleep time as 1ms durring the key loop to improve overall cache performace during high load
+// We set up a minimum sleep time as 1000ns durring the key loop to improve overall cache performace during high load
 func (sc *SyncmapCache) vaccum() {
 	log.Info("cache expire time: ", sc.expireDuration)
-	vaccumCycleDurationMs := min(int64(60000), sc.expireDuration.Milliseconds())
-	minimumSleepMs := int64(1)
+	vaccumCycleSleepMs := min(int64(10000), sc.expireDuration.Milliseconds())
+	vaccumKeyLoopSleepNs := int64(1000)
 	go func() {
 		for {
+			sc.m.Range(func(key, v interface{}) bool {
+				value := v.(Value)
+				if time.Now().After(value.timestamp.Add(sc.expireDuration)) {
+					log.Debug("deleting key:", key)
+					// This should be the only place counter decreased
+					sc.m.Delete(key)
+					sc.c.dec()
+					cacheSizeGauge.Dec()
+					log.Debug("vaccum key-loop sleep: ", vaccumKeyLoopSleepNs, "ns")
+					time.Sleep(time.Duration(vaccumKeyLoopSleepNs) * time.Nanosecond) // sleep here for some time to reduce loop frequency
+				}
+				return true
+			})
+
+			// Adjust size for counter and metric gauge
 			size := sc.SizePrecise()
 			log.Info("Vaccum cycle: Cache size from loop: ", size)
 			log.Info("Vaccum cycle: Cache size from counter: ", sc.c.countSafe())
@@ -100,41 +115,10 @@ func (sc *SyncmapCache) vaccum() {
 			sc.c.setSafe(size)
 			cacheSizeGauge.Set(float64(size))
 
-			var sleepMs int64
-			if size <= 1 {
-				sleepMs = vaccumCycleDurationMs
-			} else {
-				sleepMs = max(vaccumCycleDurationMs/(int64)(size), minimumSleepMs) // Without slowing down (setting a max 10ms sleep time) it can reduce performace with many key expires at the same time
-			}
-
-			if size == 0 {
-				log.Debug("vaccum sleep: ", sleepMs, "ms")
-				time.Sleep(time.Duration(sleepMs) * time.Millisecond)
-			} else {
-				sc.m.Range(func(key, v interface{}) bool {
-					value := v.(Value)
-					if time.Now().After(value.timestamp.Add(sc.expireDuration)) {
-						log.Debug("deleting key:", key)
-						// This should be the only place counter decreased
-						sc.m.Delete(key)
-						sc.c.dec()
-						cacheSizeGauge.Dec()
-					}
-
-					log.Debug("vaccum key-loop sleep: ", sleepMs, "ms")
-					time.Sleep(time.Duration(sleepMs) * time.Millisecond) // sleep here for some time to reduce loop frequency
-					return true
-				})
-			}
+			log.Debug("vaccum sleep: ", vaccumCycleSleepMs, "ms")
+			time.Sleep(time.Duration(vaccumCycleSleepMs) * time.Millisecond) // sleep here for some time to reduce loop frequency
 		}
 	}()
-}
-
-func max(x, y int64) int64 {
-	if x < y {
-		return y
-	}
-	return x
 }
 
 func min(x, y int64) int64 {
